911e49fa9e079962fe2a7296cc4e5709a7f38514
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2023-01-23 02:50:31 -0500

    Some final touch-ups to the directories.

--- BoardLayout.py
+++ BoardLayout.py
@@ -1,7 +1,6 @@
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QLabel, QGridLayout
 from PyQt6.QtGui import QPixmap
-from EmptyWidget import EmptyWidget
 import assets # NOTE: don't forget that :/<prefix>/<filename in qrc>
               # To compile the qrc, use pyside6-rcc <qrc filename>.qrc -o <desired python filename>.py
               # After that, don't forget to change the import: import PySide6 -> import PyQt6
--- EmptyWidget.py
+++
@@ -1,5 +0,0 @@
-from PyQt6 import QtCore, QtGui, QtWidgets
-
-class EmptyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
