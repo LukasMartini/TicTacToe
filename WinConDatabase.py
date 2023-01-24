be4787a64ad2560c07da26aa10d3e6ac3bc13172
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2023-01-24 09:49:09 -0500

    Added additional comments

--- WinConDatabase.py
+++ WinConDatabase.py
@@ -5,44 +5,13 @@ import assets
 
 class WinConDBControl():
     def __init__(self, dbfile):
+        # Set up connection to the database.
         self.conn = "God knows I'm trying, and God knows as well as I do that it's not enough."
         try:
             filepath = QCoreApplication.applicationDirPath()
             self.conn = sql.connect(filepath + "WinCon.db")
         except Error as e:
             print("Database Connection Error: File may be missing.")
-            print(e)
-
-    def initAdjacencyTable(self):
-        try:
-            curs = self.conn.cursor()
-
-            create = """CREATE TABLE IF NOT EXISTS adjacency (
-                        sec_id integer, 
-                        horizAdj1id integer, 
-                        horizAdj2id integer, 
-                        vertAdj1id integer,
-                        vertAdj2id integer)"""
-            curs.execute(create)
-
-            curs.execute("DELETE FROM adjacency") # Again, this is to clear the table to prevent appending
-
-            insert = "INSERT INTO adjacency(sec_id, horizAdj1id, horizAdj2id, vertAdj1id, vertAdj2id) VALUES(?,?,?,?,?)"
-
-            # I know there's totally a more efficient way to do this, but my sec_id system is trash and I don't have a lot of time.
-            curs.execute(insert, (0, 1, 2, 10, 20))
-            curs.execute(insert, (1, 0, 2, 11, 12))
-            curs.execute(insert, (2, 0, 1, 21, 22))
-            curs.execute(insert, (10, 11, 12, 0, 20))
-            curs.execute(insert, (11, 10, 12, 1, 21))
-            curs.execute(insert, (12, 10, 11, 2, 22))
-            curs.execute(insert, (20, 21, 22, 0, 10))
-            curs.execute(insert, (21, 20, 22, 1, 11))
-            curs.execute(insert, (22, 20, 21, 2, 12))
-
-            self.conn.commit()
-
-        except Error as e:
             print(e)
 
     def initLookUpTable(self):
@@ -97,12 +66,15 @@ class WinConDBControl():
 
     def checkWin(self, player, sec_id):
         try:
+            
             curs = self.conn.cursor()
             checkData = []
+            # Grabs all data from the tables
             fullData = []
             fullData.append(curs.execute("SELECT currStatus FROM columnZero").fetchall())
             fullData.append(curs.execute("SELECT currStatus FROM columnOne").fetchall())
             fullData.append(curs.execute("SELECT currStatus FROM columnTwo").fetchall())
+            # Checks if any of the sections haven't been clicked yet. This is used to check for a tie
             allFull = 1
             for each in fullData:
                 for peach in each:
@@ -110,6 +82,7 @@ class WinConDBControl():
                         allFull = 0
 
 
+            # Brute force check to see if there is a win
             if sec_id == 0:
                 checkData.append(curs.execute("SELECT currStatus FROM columnOne WHERE sec_id = ?", (1,)).fetchall()[0][0])
                 checkData.append(curs.execute("SELECT currStatus FROM columnTwo WHERE sec_id = ?", (2,)).fetchall()[0][0])
@@ -118,11 +91,11 @@ class WinConDBControl():
                 checkData.append(curs.execute("SELECT currStatus FROM columnOne WHERE sec_id = ?", (11,)).fetchall()[0][0])
                 checkData.append(curs.execute("SELECT currStatus FROM columnTwo WHERE sec_id = ?", (22,)).fetchall()[0][0])
                 if (checkData[0] == player and checkData[1] == player) or (checkData[2] == player and checkData[3] == player) or (checkData[4] == player and checkData[5] == player):
-                    return player
+                    return player # If any of the win methods are met, return the current player number
                 elif allFull == 1:
-                    return -2
+                    return -2 # If all of the sections have been used, return the marker of a tie
                 else:
-                    return -1
+                    return -1 # Otherwise return the marker to continue play.
             elif sec_id == 1:
                 checkData.append(curs.execute("SELECT currStatus FROM columnZero WHERE sec_id = ?", (0,)).fetchall()[0][0])
                 checkData.append(curs.execute("SELECT currStatus FROM columnTwo WHERE sec_id = ?", (2,)).fetchall()[0][0])
